7d646bbe25e8176458df61092c8144abcba3cba9
Gromaster <zysko.marek@gmail.com>
2019-03-24 18:30:16 +0100

    Ready to test

--- src/main/java/NeuralNetwork.java
+++ src/main/java/NeuralNetwork.java
@@ -25,16 +25,56 @@ class NeuralNetwork {
                 e.printStackTrace();
             }
             for(int learnSetIterations=1000;learnSetIterations<5000;learnSetIterations+=1000){
+                System.out.printf("\n~~~~~~~~~~~~~~~~~~~~~~~~\nLearnset number of epochs: %d",learnSetIterations);
                 learn(learnData.getPatientData(),learnData.getPatientDiagnoses(),learnSetIterations,true);
                 test(testData.getPatientData(),testData.getPatientDiagnoses());
             }
 
-
-
         }
     }
 
-    private void test(double[][] patientData, double[][] patientDiagnoses) {
+    private void test(double[][] patientData, double[][] patientDiagnosis) {
+
+        double[][] Results          = np.add(np.dot(weights, patientData), biases);
+        double[][] ActivationFunc   = np.sigmoid(Results);
+        double cost                 = np.cross_entropy(getLayersNumber(), patientDiagnosis, ActivationFunc);
+        double[][] Validation=null;
+        Validation=resultsComparison(ActivationFunc,patientDiagnosis);
+
+        System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~\nValidation phase");
+        System.out.println("Cost = "+cost);
+        System.out.println("Results:");
+        System.out.printf("%10s\t%10s\t%15s\t%30s\t\n","Acc","Sensivity","False alarm","Positive predictivity value");
+        for(int i=0;i<Validation.length;i++)
+            System.out.printf("%10f\t%10f\t%15f\t%30f\t",Validation[i][0],Validation[i][1],Validation[i][2],Validation[i][3]);
+     }
+
+    private double[][] resultsComparison(double[][] NNdiagnoze, double[][] patientState) {
+        double[][] results = new double[patientState[0].length][4];
+        int[] TP = new int[patientState[0].length];
+        int[] FP = new int[patientState[0].length];
+        int[] TN = new int[patientState[0].length];
+        int[] FN = new int[patientState[0].length];
+         for(int i=0;i<patientState[0].length;i++)
+             for (int j = 0; j < patientState.length; j++) {
+                 NNdiagnoze[j][i] = Math.round(NNdiagnoze[j][i]);
+                 if (NNdiagnoze[j][i] == 1.0 && patientState[j][i] == 1.0) {//TP
+                     TP[i]++;
+                 } else if (NNdiagnoze[j][i] == 1.0 && patientState[j][i] == 0.0) {//FP
+                     FP[i]++;
+                 } else if (NNdiagnoze[j][i] == 0.0 && patientState[j][i] == 1.0) {//FN
+                     FN[i]++;
+                 } else if (NNdiagnoze[j][i] == 0.0 && patientState[j][i] == 0.0) {//TN
+                     TN[i]++;
+                 }
+             }
+         for(int i=0;i<TP.length;i++){
+             results[i][0]=(double)(TP[i]+TN[i])/(double)(TP[i]+TN[i]+FP[i]+FN[i]);//ACCURACY
+             results[i][1]=(double)TP[i]/(double)(TP[i]+FN[i]);//SENSITIVITY
+             results[i][2]=(double)FP[i]/(double)(TN[i]+FP[i]);//FALSE ALARM
+             results[i][3]=(double)TP[i]/(double)(TP[i]+FP[i]);//POSITIVE PREDICTIVITY VALUE
+         }
+        return np.multiply(100,np.divide(results,patientState.length));
 
     }
 
@@ -72,6 +112,7 @@ class NeuralNetwork {
         double[][] AccumulatorOfBiases=null;
 
         for(int i=0;i<iterations;i++) {
+
             //Forward Propagation
             double[][] Results          = np.add(np.dot(weights, patientData), biases);
             double[][] ActivationFunc   = np.sigmoid(Results);
@@ -101,7 +142,7 @@ class NeuralNetwork {
             weights = np.subtract(weights, np.multiply(alfa, AccumulatorOfWeights));
             biases  = np.subtract(biases, np.multiply(alfa, AccumulatorOfBiases));
             if(i%(iterations/10)==0){
-                System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~\nLearning phase");
                 System.out.println("Cost = "+cost);
                 System.out.println("Predictions = " +Arrays.deepToString(ActivationFunc));
             }
